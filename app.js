4858f63bbd00d3fc9d5f168a0d5801487284b82b
Iris Oliver <iris.oliver@gmail.co>
2019-01-19 17:30:21 -0500

    implemented rough version of displaying todo list

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+// TODO: make the link more private somehow
+var mongoURL = "mongodb://heroku_dl6q3j4d:heroku_dl6q3j4d@ds157574.mlab.com:57574/heroku_dl6q3j4d";
+mongoose.connect(mongoURL, { useNewUrlParser: true }, function(error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(mongoose.connection.readyState);
+        console.log('connected! should be 1');
+    }
+});
+
+// Common model, will be exported to all mongoose objects
+var Todo = mongoose.model('Todo', {text: String});
+
+// Different routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var showListRouter = require('./routes/showList');
 var addItemRouter = require('./routes/addItem');
 var deleteItemRouter = require('./routes/deleteItem');
 
-var mongoURL = "mongodb://heroku_dl6q3j4d:heroku_dl6q3j4d@ds157574.mlab.com:57574/heroku_dl6q3j4d";
-mongoose.connect(mongoURL, { useNewUrlParser: true }, function(error) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(mongoose.connection.readyState);
-    console.log('connected! should be 1');
-  }
-});
-
-
-
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,19 +46,18 @@ app.use('/deleteItem', deleteItemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
-
--- routes/showList.js
+++ routes/showList.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo');
+
+// Show the list of the users
 router.get('/', function(req, res, next) {
-    res.send('showlist works');
+
+    Todo.find(function (err, todos) {
+        if (err) {
+            console.log(err);
+        }
+
+        console.log(todos);
+        res.send(todos);
+    });
+
 });
 
 module.exports = router;
