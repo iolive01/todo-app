a2b03365178be2f33713093d899a38bbab724ff4
Iris Oliver <iris.oliver@gmail.co>
2019-01-29 10:18:02 -0500

    cleaned up todo list display, connected form entry to mongo

--- public/angularCore.js
+++ public/angularCore.js
@@ -3,15 +3,19 @@ var todoAppAng = new angular.module('todoAppAng', []);
 
 todoAppAng.controller('mainController', function($scope, $http) {
     $scope.test = 'the seaport is cool!!!';
-
+    $scope.formData = 'you have not submitted yet!';
     $http.get('/showList').
         then(function(response) {
             console.log(response);
             console.log('hi');
-            $scope.todos = response;
+            $scope.todos = response.data;
         });
-        // .error(function(data) {
-        //     console.log('bye');
-        //     console.log('error with angular stuff', data);
-        // });
+
+    $scope.createTodo = function() {
+        $http.post('/addItem', {"text": $scope.formData}).success(function(data) {
+            console.log('inside success', data);
+        });
+
+    }
+
 });
--- routes/addItem.js
+++ routes/addItem.js
@@ -8,9 +8,10 @@ var Todo = mongoose.model('Todo');
 router.post('/', function(req, res, next) {
     // res.send('inside addItem');
     console.log('posting the new todo list item');
+    console.log('request:', req.body.text);
    // var newTodo = { text: req.body.text };
     Todo.create({text: req.body.text}, function(err) {
-        console.log("req body text", req.body);
+        console.log("req body text", req.body.text);
         if (err) console.log(err);
     });
 
